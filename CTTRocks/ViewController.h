58c255bb49539d468e7022acfe63ad3ecd70bdc8
jhilbert <jhilbert@aon.at>
2014-02-11 09:31:29 -0600

    init

---
+++ CTTRocks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CTTRocks
+//
+//  Created by Josef Hilbert on 11.02.14.
+//  Copyright (c) 2014 Josef Hilbert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
